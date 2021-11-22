59e86ba253632c1940f8a836f2d789892e932363
AnonymousCL1 <92658233+AnonymousCL1@users.noreply.github.com>
2021-11-22 01:51:57 -0300

    Add files via upload

---
+++ contactform/contactform.php
@@ -0,0 +1,15 @@
+<?php
+$name = $_POST["nombre"];
+$mail = $_POST["email"];
+$mensaje = $_POST["mensaje"];
+
+echo 'mensaje enviado correctamente';
+echo " El nombre recibido es: ". $name . "<br/>";
+echo " El correo electronico recibido es: ". $mail . "<br/>";
+echo " El mensaje recibido es: ". $mensaje . "<br/>";
+
+
+
+?>
+
+	
